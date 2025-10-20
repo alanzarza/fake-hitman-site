0686f01a08f095a97d80869a1bccc8ce2ea3628b
Alan Zarza <tvandmovies300@gmail.com>
2025-10-19 18:25:50 -0700

    basic homepage and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import hitmanLogo from '/hitman.svg'
 import './App.css'
 
 function App() {
@@ -9,24 +10,27 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://youtu.be/dQw4w9WgXcQ?si=DoNW6REtAiu55rGT" target="_blank">
+          <img src={hitmanLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Hitman4Hire</h1>
       </div>
-      <h1>Vite + React</h1>
+      <br/>
+      <h2>Voted best hitman hiring service by the New York times, Washington post, and X(the everything app)</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Hire now
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Hitman4Hire has been operating in the United States since 1945, originally as an independent contractor for the CIA. We have since branched off to do our own beheading and coup de tat operations throughout the globe, offering competitive rates that even the United States Government can't compete with. We democratize terror by bringing hitman operations into the hands of everyday American Citizens.
+        </p>
+        <p>
+          - Manik E.
+          CEO
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Hitman4Hire is not responsible for any accidental injuries or deaths
       </p>
     </>
   )
